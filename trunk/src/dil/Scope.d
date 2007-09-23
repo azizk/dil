c0fcae9547309d0d1f01ea3364397e9b1ced5b8b
Aziz Köksal <aziz.koeksal@gmail.com>
2007-09-23 21:50:27 +0200

    Added modules dil.Scope and dil.Symbol.

---
+++ trunk/src/dil/Scope.d
@@ -0,0 +1,11 @@
+/++
+  Author: Aziz Köksal
+  License: GPL3
++/
+module dil.Scope;
+import common;
+
+class Scope
+{
+
+}
---
+++ trunk/src/dil/Symbol.d
@@ -0,0 +1,11 @@
+/++
+  Author: Aziz Köksal
+  License: GPL3
++/
+module dil.Symbol;
+import common;
+
+class Symbol
+{
+
+}
