f068822b65c8bea75d15c08426491a65480cc6c7
Aziz Köksal <aziz.koeksal@gmail.com>
2007-10-16 22:13:37 +0200

    Translator: added new modules; made fixes and many additions.

--- trunk/src/translator/langfile.py
+++ trunk/src/translator/langfile.py
@@ -1,51 +1,55 @@
 # -*- coding: utf-8 -*-
 # Author: Aziz Köksal
 # License: GPL2
-import exceptions
 import yaml
+from errors import LoadingError
 
 class LangFile:
-  class LoadingError(exceptions.Exception):
-    def __init__(self, msg):
-      self.msg = msg
-      return
-    def __str__(self):
-      return self.msg
-
   def __init__(self, filePath):
+    self.filePath = filePath
     # Load language file and check data integrity.
     doc = yaml.load(open(filePath, "r"))
     self.doc = doc
     self.checkType(doc, dict)
     try:
-      self.langCode = doc["LangCode"]
-      self.authors = doc["Authors"]
-      self.license = doc["License"]
-      self.messages = doc["Messages"]
+      self.langCode = str(doc["LangCode"])
+      self.authors = list(doc["Authors"])
+      self.license = unicode(doc["License"])
+      self.messages = list(doc["Messages"])
     except KeyError, e:
       raise LoadingError("Missing member '%s' in '%s'" % (e.message, filePath))
 
-    self.checkType(self.langCode, dict)
-    self.checkType(self.authors, list)
+    authors = []
     for author in self.authors:
-      self.checkType(author, list)
-      if len(author) != 2:
-        raise LoadingError("")
-      self.checkType(author[0], str)
-      self.checkType(author[1], str)
-    self.checkType(self.license, str)
-    self.checkType(self.messages, list)
+      author = list(author)
+      author_len = len(author)
+      if author_len == 0:
+        pass
+      elif author_len == 1:
+        authors += [unicode(author[0]), ""]
+      else:
+        authors += [unicode(author[0]), unicode(author[1])]
+    self.authors = authors
+
+    messages = []
     for msg in self.messages:
-      self.checkType(msg, dict)
-      if not msg.has_key("ID") or \
-         not msg.has_key("Text") or \
-         not msg.has_key("Annotation") or \
-         not msg.has_key("LastEdited"):
-        raise LoadingError("")
+      self.checkType(msg, dict, "LangFile: messages must be of type dict.")
+      try:
+        msg["ID"] = int(msg["ID"])
+        msg["Text"] = unicode(msg["Text"])
+        msg["Annot"] = unicode(msg["Annot"])
+        msg["LastEd"] = unicode(msg["LastEd"])
+      except KeyError, e:
+        raise LoadingError("LangFile: a message is missing the '%s' key." % str(e))
+      messages += [msg]
+    self.messages = messages
 
   def checkType(self, var, type_, msg=""):
     if not isinstance(var, type_):
       raise LoadingError(msg)
+
+  def save(self):
+    pass
 
   def newLangFile(langCode, authors, license):
     return {
