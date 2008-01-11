dbc8dc0dfa3ded3b26466282afc3b869aca4863a
Aziz Köksal <aziz.koeksal@gmail.com>
2008-01-11 15:09:58 +0100

    Added a test case to forward02.d.

--- trunk/src/test/forward02.d
+++ trunk/src/test/forward02.d
@@ -6,4 +6,8 @@
 struct A { B* b; }
 struct B { A a; }
 // Equivalent to:
-struct A { A* a }
+struct A { A* a; }
+
+// Valid circular composition because classes are reference types.
+class C { D d; }
+class D { C c; }
