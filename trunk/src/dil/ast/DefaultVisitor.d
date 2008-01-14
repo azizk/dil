d8fa4f9e2aeb3224cfbf2f136ce5f648b1e35056
Aziz Köksal <aziz.koeksal@gmail.com>
2008-01-14 14:18:23 +0100

    Added module dil.ast.DefaultVisitor.

---
+++ trunk/src/dil/ast/DefaultVisitor.d
@@ -0,0 +1,18 @@
+/++
+  Author: Aziz Köksal
+  License: GPL3
++/
+module dil.ast.DefaultVisitor;
+
+import dil.ast.Node;
+import dil.ast.Declarations,
+       dil.ast.Expressions,
+       dil.ast.Statements,
+       dil.ast.Types,
+       dil.ast.BaseClass,
+       dil.ast.Parameters;
+
+class DefaultVisitor : Visitor
+{
+  // TODO: implement visit methods to traverse syntax tree.
+}
