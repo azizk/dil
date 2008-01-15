be1c56b29636ba7827262d5a0049cc45e1a7073d
Aziz Köksal <aziz.koeksal@gmail.com>
2008-01-15 12:10:46 +0100

    Added test forward05.d.

--- trunk/src/tests/forward03.d
+++ trunk/src/tests/forward03.d
@@ -5,3 +5,14 @@
 // Impossible static circular reference.
 const x = y;
 const y = x;
+
+// Impossible static circular reference.
+struct A
+{ const int a = B.b; }
+struct B
+{ const int b = A.a; }
+
+struct C
+{
+  const x = C.x;
+}
---
+++ trunk/src/tests/forward05.d
@@ -0,0 +1,9 @@
+/++
+  Author: Aziz Köksal
++/
+
+struct A
+{ int a = B.x; }
+
+struct B
+{ const int x = 1; }
