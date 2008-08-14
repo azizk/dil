aa64615c002bb4c92477f4c0cf3dce3803e5c38f
Aziz Köksal <aziz.koeksal@gmail.com>
2008-08-14 03:14:43 +0200

    Consolidated type rules table generation code into one file.

--- src/TypeRules.d
+++ src/TypeRules.d
@@ -1,72 +1,13 @@
-#! /usr/bin/rdmd
+#!/usr/bin/rdmd
 /++
   Author: Aziz Köksal
   License: GPL3
 +/
 module TypeRules;
-import TypeRulesData;
-import cmd.Highlight : xml_escape;
 
-
-import common;
+import tango.io.Stdout;
 
 void main(char[][] args)
-{
-  genHTMLTypeRulesTables();
-}
-
-static const string[] basicTypes = [
-  "char"[],   "wchar",   "dchar", "bool",
-  "byte",   "ubyte",   "short", "ushort",
-  "int",    "uint",    "long",  "ulong",
-  /+"cent",   "ucent",+/
-  "float",  "double",  "real",
-  "ifloat", "idouble", "ireal",
-  "cfloat", "cdouble", "creal"/+, "void"+/
-];
-
-static const string[] unaryExpressions = [
-  "!x",
-  "&x",
-  "~x",
-  "+x",
-  "-x",
-  "++x",
-  "--x",
-  "x++",
-  "x--",
-];
-
-static const string[] binaryExpressions = [
-  "x!<>=y",
-  "x!<>y",
-  "x!<=y",
-  "x!<y",
-  "x!>=y",
-  "x!>y",
-  "x<>=y",
-  "x<>y",
-
-  "x=y", "x==y", "x!=y",
-  "x<=y", "x<y",
-  "x>=y", "x>y",
-  "x<<=y", "x<<y",
-  "x>>=y","x>>y",
-  "x>>>=y", "x>>>y",
-  "x|=y", "x||y", "x|y",
-  "x&=y", "x&&y", "x&y",
-  "x+=y", "x+y",
-  "x-=y", "x-y",
-  "x/=y", "x/y",
-  "x*=y", "x*y",
-  "x%=y", "x%y",
-  "x^=y", "x^y",
-  "x~=y",
-  "x~y",
-  "x,y"
-];
-
-void genHTMLTypeRulesTables()
 {
   Stdout(
     `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">`\n
@@ -82,9 +23,10 @@ void genHTMLTypeRulesTables()
     `  </style>`\n
     `</head>`\n
     `<body>`\n
-    `<p>The following tables show the type results of different expressions. Compiler used: ` ~
-    compilerNameVersion ~ `.`
+    `<p>The following tables show the type results of different expressions. Compiler used: `
   );
+
+  Stdout.format("{} {}.{,:d3}.</p>\n", __VENDOR__, __VERSION__/1000, __VERSION__%1000);
 
   Stdout.format("<table>\n<tr><th colspan=\"{}\">Unary Expressions</th></tr>\n", unaryExpressions.length);
   Stdout("<tr><td><!--typecol--></td>");
@@ -134,3 +76,145 @@ void genHTMLTypeRulesTables()
     "\n</html>"
   );
 }
+
+/// Escapes the characters '<', '>' and '&' with named character entities.
+/// Taken from module cmd.Highlight;
+char[] xml_escape(char[] text)
+{
+  char[] result;
+  foreach(c; text)
+    switch(c)
+    {
+      case '<': result ~= "&lt;";  break;
+      case '>': result ~= "&gt;";  break;
+      case '&': result ~= "&amp;"; break;
+      default:  result ~= c;
+    }
+  if (result.length != text.length)
+    return result;
+  // Nothing escaped. Return original text.
+  delete result;
+  return text;
+}
+
+char char_; wchar wchar_; dchar dchar_; bool bool_;
+byte byte_; ubyte ubyte_; short short_; ushort ushort_;
+int int_; uint uint_; long long_; ulong ulong_;
+/+cent cent_;   ucent ucent_;+/
+float float_; double double_; real real_;
+ifloat ifloat_; idouble idouble_; ireal ireal_;
+cfloat cfloat_; cdouble cdouble_; creal creal_;
+
+static const char[][] basicTypes = [
+  "char"[],   "wchar",   "dchar", "bool",
+  "byte",   "ubyte",   "short", "ushort",
+  "int",    "uint",    "long",  "ulong",
+  /+"cent",   "ucent",+/
+  "float",  "double",  "real",
+  "ifloat", "idouble", "ireal",
+  "cfloat", "cdouble", "creal"/+, "void"+/
+];
+
+static const char[][] unaryExpressions = [
+  "!x",
+  "&x",
+  "~x",
+  "+x",
+  "-x",
+  "++x",
+  "--x",
+  "x++",
+  "x--",
+];
+
+static const char[][] binaryExpressions = [
+  "x!<>=y",
+  "x!<>y",
+  "x!<=y",
+  "x!<y",
+  "x!>=y",
+  "x!>y",
+  "x<>=y",
+  "x<>y",
+
+  "x=y", "x==y", "x!=y",
+  "x<=y", "x<y",
+  "x>=y", "x>y",
+  "x<<=y", "x<<y",
+  "x>>=y","x>>y",
+  "x>>>=y", "x>>>y",
+  "x|=y", "x||y", "x|y",
+  "x&=y", "x&&y", "x&y",
+  "x+=y", "x+y",
+  "x-=y", "x-y",
+  "x/=y", "x/y",
+  "x*=y", "x*y",
+  "x%=y", "x%y",
+  "x^=y", "x^y",
+  "x~=y",
+  "x~y",
+  "x,y"
+];
+
+template ExpressionType(alias x, alias y, char[] expression)
+{
+  static if(is(typeof(mixin(expression)) ResultType))
+    const char[] result = ResultType.stringof;
+  else
+    const char[] result = "Error";
+}
+alias ExpressionType EType;
+
+char[] genBinaryExpArray(char[] expression)
+{
+  char[] result = "[\n";
+  foreach (t1; basicTypes)
+  {
+    result ~= "[\n";
+    foreach (t2; basicTypes)
+      result ~= `EType!(`~t1~`_, `~t2~`_, "`~expression~`").result,`\n;
+    result[result.length-2] = ']'; // Overwrite last comma.
+    result[result.length-1] = ','; // Overwrite last \n.
+  }
+  result[result.length-1] = ']'; // Overwrite last comma.
+  return result;
+}
+// pragma(msg, mixin(genBinaryExpArray("x%y")).stringof);
+
+char[] genBinaryExpsArray()
+{
+  char[] result = "[\n";
+  foreach (expression; binaryExpressions)
+  {
+    result ~= genBinaryExpArray(expression);
+    result ~= ",\n";
+  }
+  result[result.length-2] = ']';
+  return result;
+}
+
+// pragma(msg, mixin(genBinaryExpsArray()).stringof);
+
+char[] genUnaryExpArray(char[] expression)
+{
+  char[] result = "[\n";
+  foreach (t1; basicTypes)
+    result ~= `EType!(`~t1~`_, int_, "`~expression~`").result,`\n;
+  result[result.length-2] = ']'; // Overwrite last comma.
+  return result;
+}
+
+char[] genUnaryExpsArray()
+{
+  char[] result = "[\n";
+  foreach (expression; unaryExpressions)
+    result ~= genUnaryExpArray(expression) ~ ",\n";
+  result[result.length-2] = ']';
+  return result;
+}
+
+// pragma(msg, mixin(genUnaryExpsArray()).stringof);
+
+auto unaryExpsResults = mixin(genUnaryExpsArray());
+auto binaryExpsResults = mixin(genBinaryExpsArray());
+
