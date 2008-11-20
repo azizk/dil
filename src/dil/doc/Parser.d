8cd571a7d39ecd9f4d4a65f98b9115f70e6eeff3
Aziz Köksal <aziz.koeksal@gmail.com>
2008-11-20 22:03:10 +0100

    Fixed DDoc Parser and added a unittest.

--- src/dil/doc/Parser.d
+++ src/dil/doc/Parser.d
@@ -29,6 +29,8 @@ struct IdentValueParser
   char* p; /// Current pointer.
   char* textEnd;
 
+  /// Parses the text into a list of IdentValues.
+  /// All newlines in text must be converted to '\n'.
   IdentValue[] parse(string text)
   {
     if (!text.length)
@@ -56,10 +58,13 @@ struct IdentValueParser
     return idvalues;
   }
 
-  /// Returns the text body. Trailing whitespace characters are not included.
+  /// Strips off leading and trailing whitespace characters.
+  /// Returns: the text body.
   char[] textBody(char* begin, char* end)
   {
-    // The body of A is empty, e.g.:
+    while (begin < end && (isspace(*begin) || *begin == '\n'))
+      begin++;
+    // The body of A is empty when e.g.:
     // A =
     // B = some text
     // ^- begin and end point to B (or to this.textEnd in the 2nd case.)
@@ -82,8 +87,9 @@ struct IdentValueParser
     while (p < textEnd)
     {
       skipWhitespace();
-      if (p >= textEnd)
+      if (p is textEnd)
         break;
+      assert(p < textEnd && (isascii(*p) || isLeadByte(*p)));
       auto idBegin = p;
       if (isidbeg(*p) || isUnicodeAlpha(p, textEnd)) // IdStart
       {
@@ -96,20 +102,21 @@ struct IdentValueParser
         if (p < textEnd && *p == '=')
         {
           p++;
-          skipWhitespace();
           bodyBegin = p;
           ident = makeString(idBegin, idEnd);
+          skipLine();
           return true;
         }
       }
       skipLine();
     }
+    assert(p is textEnd);
     return false;
   }
 
   void skipWhitespace()
   {
-    while (p < textEnd && (isspace(*p) || *p == '\n'))
+    while (p < textEnd && isspace(*p))
       p++;
   }
 
@@ -117,7 +124,8 @@ struct IdentValueParser
   {
     while (p < textEnd && *p != '\n')
       p++;
-    p++;
+    while (p < textEnd && *p == '\n')
+      p++;
   }
 }
 
@@ -126,4 +134,48 @@ char[] makeString(char* begin, char* end)
 {
   assert(begin && end && begin <= end);
   return begin[0 .. end - begin];
+}
+
+unittest
+{
+  Stdout("Testing dil.doc.Parser.\n");
+  char[] text = "A =
+B = text
+C =
+ <b>text</b>
+  D = $(LINK www.dil.com)
+E=<
+F = G = H
+Äş=??
+A6İ=µ
+End=";
+
+  IdentValue iv(string s1, string s2)
+  {
+    return new IdentValue(s1, s2);
+  }
+
+  auto results = [
+    iv("A", ""),
+    iv("B", "text"),
+    iv("C", "<b>text</b>"),
+    iv("D", "$(LINK www.dil.com)"),
+    iv("E", "<"),
+    iv("F", "G = H"),
+    iv("Äş", "??"),
+    iv("A6İ", "µ"),
+    iv("End", ""),
+  ];
+
+  auto parser = IdentValueParser();
+  foreach (i, parsed; parser.parse(text))
+  {
+    auto expected = results[i];
+    assert(parsed.ident == expected.ident,
+           Format("Parsed ident '{}', but expected '{}'.",
+                  parsed.ident, expected.ident));
+    assert(parsed.value == expected.value,
+           Format("Parsed value '{}', but expected '{}'.",
+                  parsed.value, expected.value));
+  }
 }
