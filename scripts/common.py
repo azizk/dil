321e408ec8a6892951de1fb2eeee9ccd632fc705
Aziz Köksal <aziz.koeksal@gmail.com>
2008-11-20 23:05:54 +0100

    Implemented read_modules_list() and generate_modules_js().

--- scripts/common.py
+++ scripts/common.py
@@ -3,7 +3,7 @@
 import os
 
 def get_module_fqn(prefix_path, filepath):
-  """ Returns the fully qualified module name of a D source file.
+  """ Returns the fully qualified module name (FQN) of a D source file.
     Note: this might not match with the module declaration
           in the source file. """
   # Remove prefix and strip off path separator.
@@ -14,6 +14,22 @@ def get_module_fqn(prefix_path, filepath):
   filepath = os.path.splitext(filepath)[0]
   # Finally replace the path separators.
   return filepath.replace(os.path.sep, '.') # E.g.: std/format -> std.format
+
+def read_modules_list(path):
+  from re import compile
+  rx = compile(r"^(?P<path>[^,]+), (?P<fqn>\w+(?:\.\w+)*)$")
+  return [match.groupdict() for match in map(rx.match, open(path)) if match]
+
+def generate_modules_js(modlist, dest_path):
+  """ Generates a JavaScript file with a list of modules FQNs. """
+  if not len(modlist):
+    return
+  f = open(dest_path, "w")
+  f.write('var modulesList = [\n  "%s"' % modlist[0]['fqn'])
+  for mod in modlist[1:]:
+    f.write(',\n  "%s"' % mod['fqn'])
+  f.write('\n]')
+  f.close()
 
 def download_jquery(dest_path, force=False,
                     url="http://code.jquery.com/jquery-latest.pack.js"):
