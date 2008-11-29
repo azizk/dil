e68eacae524dce72a30ccfcf1d0ee0f5bcbd19b7
Aziz Köksal <aziz.koeksal@gmail.com>
2008-11-29 20:52:10 +0100

    Cleaned up CompilerInfo for automatic updating of version info.

--- src/dil/CompilerInfo.d
+++ src/dil/CompilerInfo.d
@@ -2,16 +2,6 @@
 /// License: GPL3
 module dil.CompilerInfo;
 
-version(D2)
-  /// The major version number of this compiler.
-  const uint VERSION_MAJOR = 2;
-else
-  /// The major version number of this compiler.
-  const uint VERSION_MAJOR = 1;
-
-/// The minor version number of this compiler.
-const uint VERSION_MINOR = 0;
-
 private char[] toString(uint x)
 {
   char[] str;
@@ -24,13 +14,21 @@ private char[] toString(uint x)
 private char[] toString(uint x, uint pad)
 {
   char[] str = toString(x);
-  if (pad < str.length)
-    return str;
-  for (uint i = pad-str.length; i; i--)
-    str = "0" ~ str;
+  if (pad > str.length)
+    for (uint i = pad-str.length; i; i--)
+      str = "0" ~ str;
   return str;
 }
 
+version(D2)
+  const uint VERSION_MAJOR_DEFAULT = 2;
+else
+  const uint VERSION_MAJOR_DEFAULT = 1;
+
+/// The major version number of this compiler.
+const uint VERSION_MAJOR = VERSION_MAJOR_DEFAULT;
+/// The minor version number of this compiler.
+const uint VERSION_MINOR = 0;
 /// The compiler version formatted as a string.
 const char[] VERSION = toString(VERSION_MAJOR)~"."~toString(VERSION_MINOR, 3);
 /// The name of the compiler.
@@ -40,7 +38,7 @@ const char[] VENDOR = "dil";
 const uint DEFAULT_ALIGN_SIZE = 4;
 
 version(DDoc)
-  const uint PTR_SIZE = 0; /// The pointer size depending on the platform.
+  const uint PTR_SIZE; /// The pointer size depending on the platform.
 else
 version(X86_64)
   const uint PTR_SIZE = 8; // Pointer size on 64-bit platforms.
