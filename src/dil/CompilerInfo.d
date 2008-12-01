1785a6e2ab56fea2a9acf1ddc27220fd34ef22ae
Aziz Köksal <aziz.koeksal@gmail.com>
2008-12-01 21:44:16 +0100

    Added module dil.Version and relocated code to it.

--- src/dil/CompilerInfo.d
+++ src/dil/CompilerInfo.d
@@ -2,41 +2,13 @@
 /// License: GPL3
 module dil.CompilerInfo;
 
-private char[] toString(uint x)
-{
-  char[] str;
-  do
-    str = cast(char)('0' + (x % 10)) ~ str;
-  while (x /= 10)
-  return str;
-}
-
-private char[] toString(uint x, uint pad)
-{
-  char[] str = toString(x);
-  if (pad > str.length)
-    for (uint i = pad-str.length; i; i--)
-      str = "0" ~ str;
-  return str;
-}
-
-version(D2)
-  const uint VERSION_MAJOR_DEFAULT = 2;
-else
-  const uint VERSION_MAJOR_DEFAULT = 1;
-
-/// The major version number of this compiler.
-const uint VERSION_MAJOR = VERSION_MAJOR_DEFAULT;
-/// The minor version number of this compiler.
-const uint VERSION_MINOR = 0;
-/// The compiler version formatted as a string.
-const char[] VERSION = toString(VERSION_MAJOR)~"."~toString(VERSION_MINOR, 3);
-/// The name of the compiler.
-const char[] VENDOR = "dil";
+public import dil.Version;
 
 /// The global, default alignment size for struct fields.
 const uint DEFAULT_ALIGN_SIZE = 4;
 
+// TODO: this needs to be in CompilationContext, to make
+//       cross-compiling possible.
 version(DDoc)
   const uint PTR_SIZE; /// The pointer size depending on the platform.
 else
---
+++ src/dil/Version.d
@@ -0,0 +1,35 @@
+/// Author: Aziz Köksal
+/// License: GPL3
+module dil.Version;
+
+private char[] toString(uint x)
+{
+  char[] str;
+  do
+    str = cast(char)('0' + (x % 10)) ~ str;
+  while (x /= 10)
+  return str;
+}
+
+private char[] toString(uint x, uint pad)
+{
+  char[] str = toString(x);
+  if (pad > str.length)
+    for (uint i = pad-str.length; i; i--)
+      str = "0" ~ str;
+  return str;
+}
+
+version(D2)
+  const uint VERSION_MAJOR_DEFAULT = 2;
+else
+  const uint VERSION_MAJOR_DEFAULT = 1;
+
+/// The major version number of this compiler.
+const uint VERSION_MAJOR = VERSION_MAJOR_DEFAULT;
+/// The minor version number of this compiler.
+const uint VERSION_MINOR = 0;
+/// The compiler version formatted as a string.
+const char[] VERSION = toString(VERSION_MAJOR)~"."~toString(VERSION_MINOR, 3);
+/// The name of the compiler.
+const char[] VENDOR = "dil";
