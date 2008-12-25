e3a73b773146acfe393ec43c16d540ef2bccdee2
Aziz Köksal <aziz.koeksal@gmail.com>
2008-12-26 00:21:42 +0100

    Kandil: shrinked icons to 16x16.

--- scripts/svg2png.sh
+++ scripts/svg2png.sh
@@ -3,6 +3,6 @@
 for svg_file in `ls kandil/img/*.svg`;
 do
   file_name=`basename $svg_file .svg`
-  # Use inkscape to export a 48x48 sized PNG.
-  inkscape $svg_file --export-png=kandil/img/$file_name.png -w48 -h48
+  # Use inkscape to export a 16x16 sized PNG.
+  inkscape $svg_file --export-png=kandil/img/$file_name.png -w16 -h16
 done
