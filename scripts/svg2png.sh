19fc958fc15f0d23ca7299120357b86106c01374
Aziz Köksal <aziz.koeksal@gmail.com>
2009-01-08 15:40:51 +0100

    Kandil: fixed resolutions of the icon images.

--- scripts/svg2png.sh
+++ scripts/svg2png.sh
@@ -3,6 +3,10 @@
 for svg_file in `ls kandil/img/*.svg`;
 do
   file_name=`basename $svg_file .svg`
-  # Use inkscape to export a 16x16 sized PNG.
-  inkscape $svg_file --export-png=kandil/img/$file_name.png -w16 -h16
+  file=kandil/img/$file_name.png
+  # Use Inkscape to export a 16x16 sized PNG.
+  inkscape $svg_file --export-png=$file -w16 -h16
+  # Use Image Magick to change the resolution of the image.
+  # Inkscape can't set the resolution and size at the same time :(
+  convert -units PixelsPerInch -density 96 $file $file
 done
