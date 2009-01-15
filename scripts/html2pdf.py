74a59bd681a739c621dfaf22d3176bb6ad0e45cd
Aziz Köksal <aziz.koeksal@gmail.com>
2009-01-15 02:10:54 +0100

    PDF: implemented 'before' and 'after' pages.
    Removed the regexp mod_fqn_rx.
    Implemented inserting a page-break-before rule for a list of modules.
    Added CSS rules to keep adjacent packages together.

--- scripts/html2pdf.py
+++ scripts/html2pdf.py
@@ -23,27 +23,33 @@ def get_symbols(html_str, module_fqn):
   return symbol_list
 
 def generate_pdf(module_files, dest, tmp, params):
-  params_default = {"pdf_title": "",
+  params_default = {
+    "pdf_title": "",
     "cover_title": "Cover Title",
     "author": "",
     "generator": "dil D compiler",
     "subject": "Programming API",
     "keywords": "D programming language",
-    "x_html": "HTML",
+    "x_html": "HTML", # HTML or XHTML.
     "css_file": "pdf.css",
-    "symlink": "",
-    "nested_toc": False}
+    "symlink": "", # Prefix for symbol links.
+    "nested_toc": False, # Use nested or flat ToC.
+    "before_files": [], # Files to put before module pages.
+    "after_files": [],  # Files to put after module pages.
+    "newpage_modules": [] # Which modules should force a page break.
+  }
   params_default.update(params)
   params = params_default
 
-  x_html = params["x_html"] # XHTML or HTML.
-  symlink = params["symlink"] # Symbol link prefix.
-  nested_TOC = params["nested_toc"] # Nested or flat module list?
+  x_html = params["x_html"]
+  symlink = params["symlink"]
+  nested_TOC = params["nested_toc"]
+  newpage_modules = params["newpage_modules"]
+  before_files = params["before_files"]
+  after_files = params["after_files"]
 
   # Define some regular expressions.
   # --------------------------------
-  # Matches the contents of the variable g_moduleFQN.
-  mod_fqn_rx = re.compile(r'\s*var g_moduleFQN = "([^"]+)";')
   # Matches the href attribute of a symbol link.
   symbol_href_rx = re.compile(r'(<a class="symbol[^"]+" name="[^"]+"'
                               r' href=)"./([^"]+)"')
@@ -51,6 +57,18 @@ def generate_pdf(module_files, dest, tmp, params):
   h1_href_rx = re.compile(r'(<h1 class="module"><a href=)"./([^"]+)"')
 
   package_tree = PackageTree() # For Table of Contents.
+
+  # Add module page-break rules to pdf.css.
+  # ---------------------------------------
+  if newpage_modules:
+    f = open(tmp/params["css_file"], "r+")
+    module_page_breaks = "\n".join([
+      "h1.module[id=m-%s] { page-break-before: always; }" % fqn
+        for fqn in newpage_modules])
+    css_text = f.read().replace("/*MODULE_PAGE_BREAKS*/", module_page_breaks)
+    f.seek(0)
+    f.write(css_text)
+    f.truncate()
 
   # Prepare the HTML fragments.
   # ---------------------------
@@ -63,11 +81,10 @@ def generate_pdf(module_files, dest, tmp, params):
     html_str = h1_href_rx.sub(r'\1"%s/\2"' % symlink, html_str)
     # Extract "#content>.module".
     start = html_str.find('<div class="module">')
-    end = html_str.rfind('</div>\n<div id="footer">')
+    end = html_str.rfind('</div>\n<div id="kandil-footer">')
     content = html_str[start:end]
     # Extract module FQN.
-    m = mod_fqn_rx.search(html_str[:start]) # Limit the text to search in.
-    module_fqn = m.group(1)
+    module_fqn = Path(html_file).namebase
     # Extract symbols list.
     symbols = get_symbols(content, module_fqn)
     # Push a tuple.
@@ -124,6 +141,7 @@ def generate_pdf(module_files, dest, tmp, params):
                      (m.fqn, m.name))
     html_doc.write("</ul>\n")
 
+  html_doc.write("<p>Module list:</p>")
   if nested_TOC: # Write a nested list of modules.
     html_doc.write("<div class='modlist nested'>")
     write_nested_TOC(package_tree.root)
@@ -137,10 +155,11 @@ def generate_pdf(module_files, dest, tmp, params):
   html_doc.write("</div>\n")
 
   # Write the HTML fragments.
-  # ----------------------------
+  # -------------------------
   def write_nested_fragments(pckg):
     for p in pckg.packages:
       html_doc.write('<h1 id="p-%s" class="package">%s</h1>\n' % ((p.fqn,)*2))
+      html_doc.write('<div>')
       if len(p.packages):
         html_doc.write("<p><b>Packages:</b> ")
         html_doc.write(", ".join(["<a href='#p-%s'>%s</a>" % (p_.fqn, p_.name)
@@ -149,16 +168,28 @@ def generate_pdf(module_files, dest, tmp, params):
       html_doc.write("<p><b>Modules:</b> ")
       html_doc.write(", ".join(["<a href='#m-%s'>%s</a>" % (m.fqn, m.name)
                                  for m in p.modules]))
-      html_doc.write("</p>\n")
+      html_doc.write("</p>\n</div>")
       write_nested_fragments(p)
     for m in pckg.modules:
       html_doc.write(m.html_str.encode("utf-8"))
+
+  if before_files:
+    html_doc.write("<div class='before_pages'>")
+    for f in before_files:
+      html_doc.write(open(f).read())
+    html_doc.write("</div>")
 
   if nested_TOC:
     write_nested_fragments(package_tree.root)
   else:
     for html_fragment, mod_fqn, symbols in html_fragments:
       html_doc.write(html_fragment.encode("utf-8"))
+
+  if after_files:
+    html_doc.write("<div class='after_pages'>")
+    for f in after_files:
+      html_doc.write(open(f).read())
+    html_doc.write("</div>")
 
   html_doc.write("</body></html>")
   html_doc.flush()
