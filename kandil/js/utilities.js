878a1d659200ab3b1509fca4331d9d5481276c40
Aziz Köksal <aziz.koeksal@gmail.com>
2009-01-31 04:36:46 +0100

    Kandil: fixed {remove,add,toggle}Class() methods.

--- kandil/js/utilities.js
+++ kandil/js/utilities.js
@@ -45,15 +45,18 @@ jQuery.extend(Element.prototype, {
     // 1. '\b(?:classes)\b' would match class names with hyphens: "name-abc".
     // 2. '(?:\s|^)(?:classes)(?:\s|$)' produces wrong matches.
     // Adding a space to both sides elegantly solves this problem.
-    var rx = RegExp(" (?:"+classes+") ", "g");
-    this.className = (" "+this.className+" ").replace(rx, " ").slice(1, -1);
+    // The look-ahead is necessary to avoid eating the space
+    // between 'a' and 'b' e.g.: " a b "
+    var rx = RegExp(" (?:"+classes+")(?= )", "g");
+    this.className = (" "+this.className+" ").replace(rx, "").slice(1, -1);
     return this;
   },
   /// Adds one or more CSS classes (separated by '|') to a node.
   addClass: function(classes) {
-    if (/*this.nodeType != 1 || */this.className == undefined) return;
     this.removeClass(classes);
-    this.className += " " + classes.replace(/\|/g, " ");
+    var className = this.className;
+    className = className ? className + " " : "";
+    this.className = className + classes.replace(/\|/g, " ");
     return this;
   },
   /// Returns true if the node has one of the classes (separated by '|').
@@ -62,12 +65,24 @@ jQuery.extend(Element.prototype, {
     return RegExp(" (?:"+classes+") ").test(" "+this.className+" ");
   },
   /// Toggles one or more CSS classes (separated by '|') of a node.
-  /// Note: $('<x class="a b"/>').toggleClass("b|c|d") -> "a", not "a c d"
+  /// Note: $('<x class="a b"/>').toggleClass("b|c|d") -> "a c d"
   toggleClass: function(classes, state) {
-    if (/*this.nodeType != 1 || */this.className == undefined) return;
-    if (typeof state != typeof true)
-      state = !this.hasClass(classes);
-    state ? this.addClass(classes) : this.removeClass(classes);
+    if (typeof state != typeof true) {
+      var className = this.className;
+      if (!className) // Just add all classes when className is empty.
+        this.addClass(classes);
+      else { // Iterate over each class separately and toggle its state.
+        className = " "+className+" ";
+        classes = classes.split("|");
+        for (var i = 0, len = classes.length; i < len; i++)
+          if(className.indexOf(" "+classes[i]+" ") != -1) // hasClass
+            this.removeClass(classes[i]);
+          else
+            this.addClass(classes[i]);
+      }
+    }
+    else // Turn classes on or off according to provided state parameter.
+      state ? this.addClass(classes) : this.removeClass(classes);
     return this;
   }
 });
