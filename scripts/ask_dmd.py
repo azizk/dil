2849857d80c8ed97bf6663c5991b0b9fa8c455cc
Aziz Köksal <aziz.koeksal@gmail.com>
2010-03-27 23:33:46 +0100

    scripts: added empty 'ask_dmd.py'; it'll analyze code and error output.

---
+++ scripts/ask_dmd.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Author: Aziz Köksal
+
+# Idea: run the compiler on pieces of source code and
+# find out what the compiler produces; that is warnings,
+# errors, object code, mangled names, etc.
