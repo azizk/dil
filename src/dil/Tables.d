3a5f812178f13a805de548477ff48aee56e90173
Aziz Köksal <aziz.koeksal@gmail.com>
2010-05-06 23:01:17 +0200

    dil: added new module 'dil.Tables'.

---
+++ src/dil/Tables.d
@@ -0,0 +1,31 @@
+/// Author: Aziz Köksal
+/// License: GPL3
+/// $(Maturity average)
+module dil.Tables;
+
+import dil.lexer.Token;
+import dil.semantic.Types;
+import common;
+
+/// A collection of tables used by the Lexer and other classes.
+class Tables
+{
+  /// Contructs a Tables object.
+  this()
+  {
+    types = new TypeTable();
+  }
+
+  alias Token.StringValue StringValue;
+  alias Token.IntegerValue IntegerValue;
+  alias Token.NewlineValue NewlineValue;
+
+  TypeTable types; /// A table for D types.
+
+  string[hash_t] strings; /// Maps strings to zero-terminated string values.
+  Float[hash_t] floats; /// Maps float strings to Float values.
+  IntegerValue*[ulong] ulongs; /// Maps a ulong to an IntegerValue.
+  /// A list of newline values.
+  /// Only instances, where 'hlinfo' is null, are kept here.
+  NewlineValue*[] newlines;
+}
