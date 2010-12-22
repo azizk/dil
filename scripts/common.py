b238b25f89b6ce9364c0700024eb67df738f145b
Aziz Köksal <aziz.koeksal@gmail.com>
2010-12-22 22:35:49 +0100

    scripts: removed function download_jquery().

--- scripts/common.py
+++ scripts/common.py
@@ -211,30 +211,6 @@ def call_read(args, **kwargs):
   kwargs = dict({'stdout':subprocess.PIPE}, **kwargs)
   return subprocess.Popen(args, **kwargs).communicate()[0]
 
-# Not in use atm.
-def download_jquery(dest_path, force=False,
-                    url="http://code.jquery.com/jquery-latest.pack.js"):
-  """ Downloads the jquery library from "url".
-    If force is True, the file is fetched regardless
-    of the "Last-Modified" header. """
-  from urllib2 import Request, urlopen, HTTPError
-  from time import gmtime, strftime
-
-  request = Request(url)
-
-  if dest_path.exists and not force: # Create a GMT date stamp.
-    date_stamp = strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime(dest_path.mtime))
-    request.add_header('If-Modified-Since', date_stamp)
-
-  try:
-    print "Downloading '%s':" % url
-    js_file = urlopen(request)      # Perform the request.
-    open(dest_path, "w").write(js_file.read()) # Write to destination path.
-    print "Done >", dest_path
-  except HTTPError, e:
-    if e.code == 304: print "Not Modified"
-    else:             print e
-
 class VersionInfo:
   from sys import version_info as vi
   vi = vi
