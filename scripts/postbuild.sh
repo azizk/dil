b34d421600d8b594dc51b790ca67a8d48341ccfa
Aziz Köksal <aziz.koeksal@gmail.com>
2010-12-23 19:48:20 +0100

    scripts: removed obsolete postbuild.* files.

--- scripts/postbuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#FILES=`echo -n data/{html.css,html_map.d,lang_de.d,lang_en.d,lang_fi.d,lang_tr.d,macros_dil.ddoc,predefined.ddoc,predefined_xml.ddoc,xml.css,xml_map.d}`
-cp data/dilconf.d bin/
-#cp $FILES bin/data/
