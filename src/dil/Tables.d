efed510e3fb0b18da656c8d967fba01406f148e0
Aziz Köksal <aziz.koeksal@gmail.com>
2011-01-19 21:01:34 +0100

    dil: added ClassTable.

--- src/dil/Tables.d
+++ src/dil/Tables.d
@@ -5,7 +5,8 @@ module dil.Tables;
 
 import dil.lexer.Token;
 import dil.lexer.IdTable;
-import dil.semantic.Types;
+import dil.semantic.Types,
+       dil.semantic.Symbols;
 import dil.Float;
 import common;
 
@@ -17,6 +18,7 @@ class Tables
   {
     idents = new IdTable();
     types = new TypeTable();
+    classes = new ClassTable();
   }
 
   alias Token.StringValue StringValue;
@@ -24,6 +26,7 @@ class Tables
   alias Token.NewlineValue NewlineValue;
 
   TypeTable types; /// A table for D types.
+  ClassTable classes; /// Special classes.
 
   // A collection of tables for various token values.
   IdTable idents;
@@ -40,4 +43,32 @@ class Tables
   {
     return idents.lookup(str);
   }
+}
+
+/// A collection of special classes.
+class ClassTable
+{
+  // Classes from object.d:
+  ClassSymbol object; /// Class Object.
+  ClassSymbol classInfo; /// Class ClassInfo.
+  ClassSymbol moduleInfo; /// Class ModuleInfo.
+  ClassSymbol exeption; /// Class Exeption.
+  // Classes from runtime library files:
+  ClassSymbol tinfo; /// Class TypeInfo.
+  ClassSymbol tinfoArray; /// Class TypeInfo_Array.
+  ClassSymbol tinfoAArray; /// Class TypeInfo_AssociativeArray.
+  ClassSymbol tinfoClass; /// Class TypeInfo_Class.
+  ClassSymbol tinfoDelegate; /// Class TypeInfo_Delegate.
+  ClassSymbol tinfoEnum; /// Class TypeInfo_Enum.
+  ClassSymbol tinfoFunction; /// Class TypeInfo_Function.
+  ClassSymbol tinfoInterface; /// Class TypeInfo_Interface.
+  ClassSymbol tinfoPointer; /// Class TypeInfo_Pointer.
+  ClassSymbol tinfoSArray; /// Class TypeInfo_StaticArray.
+  ClassSymbol tinfoStruct; /// Class TypeInfo_Struct.
+  ClassSymbol tinfoTuple; /// Class TypeInfo_Tuple.
+  ClassSymbol tinfoTypedef; /// Class TypeInfo_Typedef.
+  // D2:
+  ClassSymbol tinfoConst; /// Class TypeInfo_Const.
+  ClassSymbol tinfoInvariant; /// Class TypeInfo_Invariant.
+  ClassSymbol tinfoShared; /// Class TypeInfo_Shared.
 }
