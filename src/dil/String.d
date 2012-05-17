bf0ad7374e70968c3ea586a61f5a8547b675f0a0
Aziz Köksal <aziz.koeksal@gmail.com>
2012-05-17 11:53:47 +0200

    lexer: moved hashOf()/hashOfCTF() to dil.String.

--- src/dil/String.d
+++ src/dil/String.d
@@ -712,6 +712,55 @@ char[] itoactf(ulong x)
   return str;
 }
 
+/// Calculates a hash value for str.
+/// Note: The value will differ between 32bit and 64bit systems.
+/// It will also differ between little and big endian systems.
+hash_t hashOf(cstring str)
+{
+  return String(str).hashOf();
+}
+
+/// ditto
+hash_t hashOfCTF(string str)
+{ /// Nested func because DMD can't do: cast(hash_t[])str
+  hash_t[] toHashArray(string s)
+  { // See: $(DMDBUG 5497, reinterpret cast inside CTFs)
+    hash_t swapBytesOnLE(hash_t c)
+    { // Reverse bytes on little-endian machines.
+      version(BigEndian)
+      return c; // Return as is on big-endian machines.
+      hash_t c_;
+      for (size_t i; i < hash_t.sizeof; i++, c >>= 8)
+        c_ = c_ << 8 | (c & 0xFF); // Pick 1byte from c and append to c_.
+      return c_;
+    }
+    auto ha_len = s.length / hash_t.sizeof;
+    hash_t[] ha;
+    for (size_t i, j; i < ha_len; i++)
+    {
+      hash_t hc; // A hash char.
+      for (size_t k = j + hash_t.sizeof; j < k; j++)
+        hc = hc << 8 | s[j]; // Append as many bytes as possible.
+      ha ~= swapBytesOnLE(hc); // Append to hash array.
+    }
+    return ha;
+  }
+
+  hash_t hash;
+  auto rem_len = str.length % hash_t.sizeof; // Remainder.
+  hash_t[] ha = toHashArray(str);
+  if (rem_len)
+  { // Append remainder hash character.
+    hash_t hc;
+    foreach (c; str[$-rem_len .. $]) // Remainder loop.
+      hc = (hc << 8) | c;
+    ha ~= hc;
+  }
+  foreach (hc; ha) // Main loop.
+    hash = hash * 11 + hc;
+  return hash;
+}
+
 unittest
 {
   scope msg = new UnittestMsg("Testing struct String.");
