57f5139db834ed836f48dd88fc3dd121234542dc
user.email <aziz.koeksal@gmail.com>
2012-07-09 22:00:22 +0200

    make_tango: added cmdline option for defining the path to Tango.

--- scripts/make_tango.py
+++ scripts/make_tango.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals, print_function
 from common import *
 from build import DMDCommand, LDCCommand
+__file__ = tounicode(__file__)
 
 # Linux:
 def make_Linux(TANGO):
@@ -14,30 +15,51 @@ def make_Linux(TANGO):
   for bits in (32, 64):
     DEST = (TANGO/"lib%d"%bits).mkdir()
     # Release
-    call_proc(bob, "-vu", "-m=%d"%bits, ".", cwd=TANGO)
+    ret = call_proc(bob, "-vu", "-m=%d"%bits, ".", cwd=TANGO)
+    if ret != 0:
+      break
     map(Path.rm, TANGO.glob("*.o"))
     (TANGO/lib).move(DEST)
     # Debug
-    call_proc(bob, "-vu", "-m=%d"%bits, ".", "-o=-g", cwd=TANGO)
+    ret = call_proc(bob, "-vu", "-m=%d"%bits, ".", "-o=-g", cwd=TANGO)
+    if ret != 0:
+      break
     map(Path.rm, TANGO.glob("*.o"))
     (TANGO/lib).move(DEST/"libtango-dmd-dbg.a")
 
 def make_Windows(TANGO):
   bob = "build/bin/win32/bob.exe"
   lib = "libtango-dmd.lib"
+  build = call_proc
+  if not is_win32:
+    build = lambda *a, **k: call_proc("wine", *a, **k)
   for bits in (32,): # 64 not supported yet.
     DEST = (TANGO/"lib%d"%bits).mkdir()
     # Release
-    call_proc("wine", "bob", "-vu", "-m=%d"%bits, ".", cwd=TANGO)
+    ret = build(bob, "-vu", "-m=%d"%bits, ".", cwd=TANGO)
+    if ret != 0:
+      break
     (TANGO/lib).move(DEST)
     # Debug
-    call_proc("wine", "bob", "-vu", "-m=%d"%bits, "-o=-g", ".", cwd=TANGO)
+    ret = build(bob, "-vu", "-m=%d"%bits, "-o=-g", ".", cwd=TANGO)
+    if ret != 0:
+      break
     (TANGO/lib).move(DEST/"libtango-dmd-dbg.lib")
 
 def main():
-  TANGO = Path("/home/aziz/git/tango")
+  from optparse import OptionParser
+
+  usage = "Usage: %s TANGO_DIR" % __file__
+  parser = OptionParser(usage=usage)
+  (opts, args) = parser.parse_args(sys.uargv[1:])
+  if len(args) < 1:
+    parser.error("missing argument TANGO_DIR")
+
+  TANGO = Path(args[0])
+
   make_Linux(TANGO)
   make_Windows(TANGO)
+
   return
 
 if __name__ == '__main__':
