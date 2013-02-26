76eff8cdcb5a388b65db83fd00e4ad254ef4fbb1
Aziz Köksal <aziz.koeksal@gmail.com>
2013-02-26 19:54:13 +0100

    scripts: refactored class Target; added class Targets.
    * Added linker flag format attributes.

--- scripts/targets.py
+++ scripts/targets.py
@@ -3,38 +3,43 @@
 # Author: Aziz Köksal
 # License: zlib/libpng
 from __future__ import unicode_literals, print_function
+from utils import dicta, dictad
 
-class Target(dict):
+class Target(dictad):
   """ Provides specific properties that apply to a target machine/platform. """
-  def __getattr__(self, name): return self.get(name, None)
-  def __setattr__(self, name, value): self[name] = value
-  def __delattr__(self, name): del self[name]
+class Targets(dicta):
+  """ Collection of Target objects. """
 
-twindows32 = Target(
+Targets = Targets()
+
+Targets.Win = Target(
   name="Windows",
   dir="windows",
   iswin=True,
-  bits=32,
   dbgexe="dil%s_dbg.exe",
   rlsexe="dil%s.exe",
   libsffx=".lib",
   objsffx=".obj",
   binsffx=".exe",
+  lnklibf="+lib%s.lib",
+  lnklibd="+%s\\",
 )
 
-twindows64 = Target(twindows32, bits=64)
+Targets.Win32 = Target(Targets.Win, bits=32)
+Targets.Win64 = Target(Targets.Win, bits=64)
 
-tlinux32 = Target(
+Targets.Lin = Target(
   name="Linux",
   dir="linux",
   islin=True,
-  bits=32,
-  arch="i386",
   dbgexe="dil%s_dbg",
   rlsexe="dil%s",
   libsffx=".a",
   objsffx=".o",
   binsffx="",
+  lnklibf="-l%s",
+  lnklibd="-L%s",
 )
 
-tlinux64 = Target(tlinux32, bits=64, arch="amd64")
+Targets.Lin32 = Target(Targets.Lin, bits=32, arch="i386")
+Targets.Lin64 = Target(Targets.Lin, bits=64, arch="amd64")
