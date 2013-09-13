2392b196390633368d4f3ecb47188d75dbbef549
Aziz Köksal <aziz.koeksal@gmail.com>
2013-09-13 17:20:34 +0200

    ast: simplified template copyMethod.

--- src/dil/ast/NodeCopier.d
+++ src/dil/ast/NodeCopier.d
@@ -3,10 +3,6 @@
 /// $(Maturity very high)
 module dil.ast.NodeCopier;
 
-import dil.ast.NodesEnum;
-
-import common;
-
 /// Provides a copy() method for subclasses of Node.
 mixin template copyMethod()
 {
@@ -14,26 +10,29 @@ mixin template copyMethod()
   { // First do a shallow copy.
     auto n = cast(typeof(this))cast(void*)this.dup;
     // Then copy each subnode.
-    foreach (i, T; _mtypes)
+    with (n) foreach (i, T; _mtypes)
     {
-      enum m = _members[i];
+      mixin("alias member = "~_members[i]~";");
       static if (is(T : Node)) // A Node?
       {
         static if (_mayBeNull[i])
-          mixin("if(n."~m~") n."~m~" = n."~m~".copy();");
+        {
+          if(member)
+            member = member.copy();
+        }
         else
-          mixin("n."~m~" = n."~m~".copy();");
+          member = member.copy();
       }
-      else static if (is(T t : E[], E) && is(E : Node)) // A Node array?
+      else
+      static if (is(T t : E[], E) && is(E : Node)) // A Node array?
       {
-        static if (_mayBeNull[i])
-          mixin("n."~m~" = n."~m~".dup;
-                  foreach (ref x; n."~m~")
-                    if (x) x = x.copy();");
-        else
-          mixin("n."~m~" = n."~m~".dup;
-                      foreach (ref x; n."~m~")
-                        x = x.copy();");
+        foreach (ref x; member)
+        {
+          static if (_mayBeNull[i])
+            if (x is null)
+              continue;
+          x = x.copy();
+        }
       }
     }
     return n;
