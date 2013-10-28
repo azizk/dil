c3f41d0d070f8965857188fb88f5b4a219e37743
Aziz Köksal <aziz.koeksal@gmail.com>
2013-10-28 19:52:52 +0100

    Array: added templated struct DArray based on the GC.

--- src/dil/Array.d
+++ src/dil/Array.d
@@ -6,6 +6,7 @@ module dil.Array;
 import core.stdc.stdlib,
        core.memory,
        core.exception;
+import std.traits : hasIndirections;
 import common;
 
 /// The system size of a memory page. Default to 4k.
@@ -20,6 +21,147 @@ static this()
   {
     import core.sys.posix.unistd;
     PAGESIZE = cast(size_t)sysconf(_SC_PAGE_SIZE);
+  }
+}
+
+/// An array implementation that uses D's Garbage Collector functions.
+struct DArray(E)
+{
+  E* ptr;
+  E* cur;
+  E* end;
+
+  /// A block needs to be scanned by the GC, if E as pointers.
+  enum scanIfPtrs = hasIndirections!E ? 0 : GC.BlkAttr.NO_SCAN;
+
+  /// Constructs a DArray and reserves space for n elements.
+  this(size_t n = 0)
+  {
+    if (n)
+      if (auto p = cast(E*)GC.malloc(n, scanIfPtrs))
+        end = (ptr = cur = p) + n;
+  }
+
+  invariant()
+  {
+    if (!ptr)
+      assert(cur is null && end is null, "!(ptr == cur == end == 0)");
+    else
+      assert(ptr <= cur && cur <= end,
+        Format("!({} <= {} <= {})", ptr, cur, end));
+  }
+
+  /// Returns the size of the array.
+  size_t len() @property const
+  {
+    return cur - ptr;
+  }
+
+  /// Sets the size of the array.
+  /// Resizes space if necessary. Does not deallocate if n is zero.
+  void len(size_t n) @property
+  {
+    if ((ptr + n) > end)
+      reserve(n);
+    cur = ptr + n;
+  }
+
+  /// Returns the remaining space before a reallocation is needed.
+  size_t rem() @property const
+  {
+    return end - cur;
+  }
+
+  /// Sets the capacity so that space for n elements remain.
+  void rem(size_t n) @property
+  {
+    cap = len + n;
+  }
+
+  /// Returns the total capacity of the array.
+  size_t cap() @property const
+  {
+    return end - ptr;
+  }
+
+  /// Sets the capacity to exactly n elements.
+  void cap(size_t n) @property
+  {
+    reserve(n);
+  }
+
+  /// Reserves memory for at least n elements.
+  void reserve(size_t n)
+  {
+    auto len = this.len;
+    len = (len < n ? len : n); // min(len, n)
+    if (!ptr || GC.extend(ptr, n, n) == 0)
+    { // Couldn't extend. Allocate new memory.
+      auto newPtr = cast(E*)GC.malloc(n * E.sizeof, scanIfPtrs);
+      if (len) // Copy to the new block if there are elements.
+         newPtr[0..len] = ptr[0..len];
+      ptr = newPtr;
+    }
+    cur = ptr + len;
+    end = ptr + n;
+  }
+
+  /// Appends x to the array.
+  /// Appends the elements if X itself is an array.
+  void opOpAssign(string op : "~", X)(const X x)
+  {
+    static if (is(typeof(cur[0] == x[0])))
+    {
+      auto n = x.length;
+      if (cur + n >= end)
+        rem = n;
+      cur[0..n] = x;
+      cur += n;
+    }
+    else
+    {
+      if (cur + 1 >= end)
+        rem = 1;
+      *cur++ = x;
+    }
+    assert(cur <= end);
+  }
+
+  /// Appends several items to the array.
+  void put(Xs...)(Xs xs)
+  {
+    foreach (x; xs)
+      this ~= x;
+  }
+
+  /// Appends several items to the array,
+  /// without checking for sufficient space.
+  void putUnsafe(Xs...)(Xs xs)
+  {
+    foreach (x; xs)
+      static if (is(typeof(cur[0] == x[0])))
+      {
+        auto n = x.length;
+        cur[0..n] = x;
+        cur += n;
+      }
+      else
+        *cur++ = x;
+    assert(cur <= end);
+  }
+
+  /// Returns a slice.
+  E[] opSlice()
+  {
+    return ptr[0..len];
+  }
+
+  alias array = opSlice;
+
+  /// Returns a copy.
+  E[] dup()
+  {
+    return this[].dup;
   }
 }
 
